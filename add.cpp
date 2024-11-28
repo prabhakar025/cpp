8614fefbd33cf76ee08e8a3ca8e51f29b3aa2882
prabhakar025 <prabhakar263801@gmail.com>
2024-11-28 23:55:13 +0530

    add two number

---
+++ add.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 10;
+    int num2 = 20;
+    int sum = num1 + num2;
+    cout << "The sum of " << num1 << " and " << num2 << " is: " << sum << endl;
+    return 0;
+}
